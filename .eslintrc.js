439d7ed91d202880b85fbec86a5f110beb29baf1
jhoneybee <zhangjin19960908@gmail.com>
2020-07-17 16:36:33 +0800

    fix: 添加pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     extends: [require.resolve('@umijs/fabric/dist/eslint')],
     rules: {
-        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+            { argsIgnorePattern: '^_' },
+        ],
     },
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
     extends: [require.resolve('@umijs/fabric/dist/stylelint')],
-    rules: {
-    },
+    rules: {},
 };
